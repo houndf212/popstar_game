20479501b049b184b039ba4c7f47e98980751677
wangchao <360327741@qq.com>
2017-03-13 16:36:54 +0800

    ...

--- starboard.h
+++ starboard.h
@@ -5,13 +5,18 @@
 #include <memory>
 #include "graphicmatrix.h"
 #include "matrixslice.h"
+#include "scorecalc.h"
 
 class StarBoard : public QObject
 {
     Q_OBJECT
 public:
-    StarBoard();
-    QGraphicsScene* get_scene() const { return m_scene.data(); }
+    explicit StarBoard(QObject* parent=nullptr);
+    QGraphicsScene* get_scene() const { return m_scene; }
+    const ScoreCalc* get_scoreptr() const { return &m_score; }
+    Q_SIGNAL void sig_over();
+    Q_SIGNAL void sig_score_changed();
+    Q_SLOT void reset();
 private:
     void gen_board();
     void gen_graphic_matrix();
@@ -21,13 +26,14 @@ private:
 private:
     Q_SLOT void onStarClicked();
 private:
+    ScoreCalc m_score;
     Matrix m_matrix;
     QScopedPointer<MatrixSlice> m_slice;
 
     GraphicMatrix m_graphic_original_matrix;
     GraphicMatrix m_graphic_current_matrix;
 
-    QScopedPointer<QGraphicsScene> m_scene;
+    QGraphicsScene* m_scene;
 private:
     typedef std::pair<Pos, Pos> MovedPos;
     static std::vector<MovedPos> find_move_pos(const Matrix& before, const Matrix& after);
